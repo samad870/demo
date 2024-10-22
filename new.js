0dbfa6c216d2a3276b2e232532c53c97c6d9baab
samad malik <samadmalikz123@gmail.com>
2024-10-22 09:22:29 -0700

    demo sessoin

---
+++ new.js
@@ -0,0 +1,42 @@
+// let abc = Math.abs(-2);
+// console.log(abc);
+// Math.round
+// Math.random
+// Math.ceil
+// Math.trunc
+// Math.sign
+// Math.pow
+// Math.sqrt
+// Math.abs
+
+// function xyz(a, b) {
+//   return Math.abs(a - b);
+// }
+// console.log(xyz(2, 5));
+
+// let b = Math.PI;
+// console.log(b);
+// let a = Math.sin((90 * b) / 180);
+// console.log(a);
+
+// let a = Math.min(2, 3, 6, 4, 3, 9);
+// console.log(a);
+
+// let b = Math.floor(Math.random() * 5 + 10);
+// console.log(b);
+
+// let s = Math.log2(512);
+// console.log(s);
+
+// let s = new Date("Mon Oct 21 2024 09:50:43");
+// console.log(s);
+
+// let a = new Date(2012, 12, 35);
+// let b = new Date(2012, 11, 12);
+// console.log(a);
+// console.log(b);
+
+let s = new Date();
+s.setHours(2);
+console.log(s);
+// console.log(s.toUTCString());
